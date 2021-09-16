a804787f7a155526bfd16eff6e5bf503e5c0c17a
Kirk Clarke <kclarke.design@gmail.com>
2021-09-16 02:55:29 -0400

    initial commit, added event listener for controls, started player class

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,19 +1,33 @@
 class Player {
-  constructor({x, y, score, id}) {
-
+  constructor({ x, y, score = 0, id }) {
+    this.x = x;
+    this.y = y;
+    this.score = score;
+    this.id = id;
   }
 
   movePlayer(dir, speed) {
-
+    // set direction based on key control
+    const directionKeyMap = { w: "up", a: "left", s: "down", d: "right" }[dir];
+    // move based on direction map
+    const movement = {
+      up: this.y + speed,
+      right: this.x + speed,
+      down: this.y - speed,
+      left: this.x - speed,
+    }[directionKeyMap];
+    // set appropriate axis
+    if (directionKeyMap == "up" || directionKeyMap == "down") {
+      this.y = movement;
+    } else {
+      this.x = movement;
+    }
+    console.log(`${this.x}, ${this.y}`);
   }
 
-  collision(item) {
+  collision(item) {}
 
-  }
-
-  calculateRank(arr) {
-
-  }
+  calculateRank(arr) {}
 }
 
 export default Player;
